f571af0ad70ca7cb8ee58800a8c7699cfa5d05b3
William Bohn <william.bohn@corsosystems.com>
2024-12-02 15:53:30 -0600

    day 1 part 1

---
+++ AdventOfCode/AbstractBaseDay.cs
@@ -0,0 +1,13 @@
+﻿namespace AdventOfCode
+{
+    public abstract class AbstractBaseDay : BaseDay
+    {
+        protected readonly string _inputText;
+        protected readonly string[] _inputLines;
+        protected AbstractBaseDay()
+        {
+            _inputText = File.ReadAllText(InputFilePath);
+            _inputLines = _inputText.Split("\r\n");
+        }
+    }
+}
--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,15 +1,31 @@
 ﻿namespace AdventOfCode;
 
-public class Day01 : BaseDay
+public class Day01 : AbstractBaseDay
 {
-    private readonly string _input;
-
-    public Day01()
+    public override ValueTask<string> Solve_1()
     {
-        _input = File.ReadAllText(InputFilePath);
-    }
+        List<int> leftList = [];
+        List<int> rightList = [];
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+        foreach (var line in _inputLines)
+        {
+            List<int> locationIds = line.Split("   ")
+                .Select(int.Parse)
+                .ToList();
+            leftList.Add(locationIds[0]);
+            rightList.Add(locationIds[1]);
+        }
+
+        var pairedLocationIds = leftList.OrderBy(n => n)
+            .Zip(rightList.OrderBy(n => n));
+
+        var distances = pairedLocationIds.Select(pair => Math.Abs(pair.First - pair.Second));
+        foreach (var (First, Second) in pairedLocationIds)
+        {
+            int distance = Math.Abs(First - Second);
+        }
+        return new ValueTask<string>(distances.Sum().ToString());
+    }
 
     public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
 }
