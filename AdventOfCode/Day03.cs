5518a0162734a1c9121efc410342d4198a174b8d
William Bohn <william.bohn@corsosystems.com>
2024-12-03 17:06:14 -0600

    day 3 part 1

---
+++ AdventOfCode/Day03.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
+public class Day03 : AbstractBaseDay
+{
+    public override ValueTask<string> Solve_1()
+    {
+        string pattern = @"mul\(\d{1,3},\d{1,3}\)";
+        MatchCollection matches = Regex.Matches(_inputText, pattern);
+
+        int sum = 0;
+        foreach (Match match in matches)
+        {
+            int x = int.Parse(match.Value.Split(",")[0][4..]);
+            int y = int.Parse(match.Value.Split(",")[1][0..^1]);
+            sum += x * y;
+        }
+        return ValueTask.FromResult(sum.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return ValueTask.FromResult("Not implemented");
+    }
+}
