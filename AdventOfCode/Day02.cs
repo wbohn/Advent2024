d7fd48f7dcc00294ac17b1bd7b7f0037fbeb16b7
William Bohn <william.bohn@corsosystems.com>
2024-12-03 16:41:45 -0600

    day 2 part 1

---
+++ AdventOfCode/Day02.cs
@@ -0,0 +1,42 @@
+﻿namespace AdventOfCode;
+public class Day02 : AbstractBaseDay
+{
+    public override ValueTask<string> Solve_1()
+    {
+        int safeCount = 0;
+        foreach (var safetyReport in _inputLines)
+        {
+            var levels = safetyReport.Split(" ")
+                .Select(int.Parse);
+            if (IsSortedSafely(levels))
+            {
+                safeCount++;
+            }
+        }
+        return new ValueTask<string>(safeCount.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+
+    static bool IsSortedSafely(IEnumerable<int> levels)
+    {
+        return IsIncreasingSafely(levels) || IsDecreasingSafely(levels);
+    }
+
+    static bool IsIncreasingSafely(IEnumerable<int> levels)
+    {
+        return levels.Zip(
+            levels.Skip(1), (a, b) => a < b && (b - a) >= 1 && (b - a) <= 3)
+            .All(x => x);
+    }
+
+    static bool IsDecreasingSafely(IEnumerable<int> levels)
+    {
+        return levels.Zip(
+            levels.Skip(1), (a, b) => a > b && (a - b) >= 1 && (a - b) <= 3)
+            .All(x => x);
+    }
+}
