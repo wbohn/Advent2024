d31c6f9f4fcbad891da6fbda01b09e639cdca036
William Bohn <william.bohn@corsosystems.com>
2024-12-03 23:38:35 -0600

    day 3 part 2

--- AdventOfCode/Day03.cs
+++ AdventOfCode/Day03.cs
@@ -3,23 +3,57 @@
 namespace AdventOfCode;
 public class Day03 : AbstractBaseDay
 {
+    const string instructionPattern = @"\bdo\(\)|\bdon't\(\)|mul\(\d{1,3},\d{1,3}\)";
+    readonly MatchCollection instructions;
+
+    public Day03()
+    {
+        instructions = Regex.Matches(_inputText, instructionPattern);
+    }
+
     public override ValueTask<string> Solve_1()
     {
-        string pattern = @"mul\(\d{1,3},\d{1,3}\)";
-        MatchCollection matches = Regex.Matches(_inputText, pattern);
-
         int sum = 0;
-        foreach (Match match in matches)
+        foreach (Match instruction in instructions)
         {
-            int x = int.Parse(match.Value.Split(",")[0][4..]);
-            int y = int.Parse(match.Value.Split(",")[1][0..^1]);
-            sum += x * y;
+            if (instruction.Value.StartsWith("mul"))
+            {
+                sum += Mul(instruction);
+            }
         }
         return ValueTask.FromResult(sum.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        return ValueTask.FromResult("Not implemented");
+        int sum = 0;
+        bool enabled = true;
+        foreach (Match instruction in instructions)
+        {
+            switch (instruction.Value)
+            {
+                case "do()":
+                    enabled = true;
+                    break;
+                case "don't()":
+                    enabled = false;
+                    break;
+                default: // mul(x,y)
+                    if (enabled)
+                    {
+                        sum += Mul(instruction);
+                    }
+                    break;
+            }
+        }
+        return ValueTask.FromResult(sum.ToString());
+    }
+
+    static int Mul(Match mulInstruction)
+    {
+        // mul(x,y)
+        int x = int.Parse(mulInstruction.Value.Split(",")[0][4..]);
+        int y = int.Parse(mulInstruction.Value.Split(",")[1][0..^1]);
+        return x * y;
     }
 }
